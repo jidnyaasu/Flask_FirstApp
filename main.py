0f7b4c55db8d0cda293f5787a43d3ecaa506725f
jidnyaasu <sbj.amb@gmail.com>
2022-05-07 17:21:18 +0530

    Added flask wtf-forms removed all other routes except root

--- main.py
+++ main.py
@@ -1,73 +1,34 @@
-import time
-from flask import Flask, g, request, jsonify, render_template
-from datetime import datetime
-
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, HiddenField, RadioField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config["secret"] = "10"
+app.config["SECRET_KEY"] = "hard to guess string"
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-
-
-@app.route('/search')
-def search():
-    return render_template('search_engine.html')
-
-
-# our temporary db
-users = []
-
-
-@app.route('/user/', methods=["GET", "POST", "DELETE"])
-@app.route('/user/<name>', methods=["GET", "POST", "DELETE"])
-def user(name=None):
-    if request.method == "GET":
-        date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-        # If name is given, lets send decorated response, otherwise return all users till now
-        if name:
-            return render_template('user.html', user=name, date=date)
-        else:
-            return render_template('user.html', user=name, users=users)
-    elif request.method == "POST" and name:
-        # I am just adding name to the in memory db
-        if name not in users:
-            users.append(name)
-    elif request.method == "DELETE":
-        # If the name found in the users, lets delete
-        if name in users:
-            users.remove(name)
-    return jsonify({})
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    password = PasswordField("Enter your password")
+    hidden = HiddenField("Text entered here will be hidden")
+    gender = RadioField("Male", "Female")
+    submit = SubmitField("Submit")
 
 
 @app.errorhandler(404)
 def not_found(e):
-    return render_template('error.html', error=e, title="404 Not Found", heading="<h1>Sorry, page not found.</h1>")
+    return render_template('error.html', error=e, title="404 Not Found", heading="<h1>Sorry, page not found.</h1>"), 404
 
 
-@app.before_request
-def before_request():
-    # in the g object lets store the current time
-    # later in after request, lets calculate total time
-    g.request_start_time = time.time()
-    g.req_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
-
-
-@app.after_request
-def after_request(response):
-    """
-    print the total req time.
-    :param response:
-    :return:
-    """
-    print(g.req_time())
-    return response
+@app.route('/', methods=["GET", "POST"])
+def index():
+    name = None
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
