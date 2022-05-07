f677729bf32b1c5eec7ea5892313a4abc48c09ad
jidnyaasu <sbj.amb@gmail.com>
2022-05-07 18:18:04 +0530

    typos corrected

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, HiddenField, RadioField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
