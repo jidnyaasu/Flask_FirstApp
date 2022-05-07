98409acef5d92504bc0600c46806a16ac4af9dc4
jidnyaasu <sbj.amb@gmail.com>
2022-05-07 17:27:35 +0530

    Added search route and 'search_engine.html'

--- main.py
+++ main.py
@@ -31,5 +31,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/search')
+def search():
+    return render_template('search_engine.html')
+
+
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
