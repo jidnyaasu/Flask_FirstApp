ebfe74508fc13fe9b069f8c77959e5d8efb793b7
jidnyaasu <sbj.amb@gmail.com>
2022-05-08 22:40:37 +0530

    Stored user's name in a session variable, used redirect after a POST request

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -6,7 +6,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "hard to guess string"
+app.config["SECRET_KEY"] = "hard-to-guess-string"
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -19,17 +19,16 @@ class NameForm(FlaskForm):
 
 @app.errorhandler(404)
 def not_found(e):
-    return render_template('error.html', error=e, title="404 Not Found", heading="<h1>Sorry, page not found.</h1>"), 404
+    return render_template('error.html', error=e), 404
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/search')
