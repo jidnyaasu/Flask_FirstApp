c407011842a6d7a9c3a967f22e9b2f1f19ce12d1
jidnyaasu <sbj.amb@gmail.com>
2022-05-23 13:28:29 +0530

    Rectified code errors

--- app/email.py
+++ app/email.py
@@ -1,20 +1,20 @@
 from threading import Thread
-
-from flask import render_template
+from flask import current_app, render_template
 from flask_mail import Message
-from social_app import app
+from . import mail
 
 
-def send_async_email(msg):
+def send_async_email(app, msg):
     with app.app_context():
-        app.mail.send(msg)
+        mail.send(msg)
 
 
 def send_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
     msg = Message(app.config["MAIL_SUBJECT_PREFIX"] + " " + subject,
                   sender=app.config["SOCIAL_APP_MAIL_SENDER"], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[msg])
+    thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
